bbbd6f34aa25c2981bb4e7ffa773398e073d8b95
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-09-05 12:11:14 +0200

    add security configurations

--- src/main/java/com/itacademy/virtualpet/service/SecurityConfig.java
+++ src/main/java/com/itacademy/virtualpet/service/SecurityConfig.java
@@ -1,2 +1,29 @@
-package com.itacademy.virtualpet.service;public class SecurityConfig {
+package com.itacademy.virtualpet.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
 }
