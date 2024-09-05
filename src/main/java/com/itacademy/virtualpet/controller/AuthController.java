fe8142a678d177e7e00534bbfdb30b0ef10059a4
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-09-05 12:11:34 +0200

    add AuthController for users to login and register

--- src/main/java/com/itacademy/virtualpet/controller/AuthController.java
+++ src/main/java/com/itacademy/virtualpet/controller/AuthController.java
@@ -1,2 +1,28 @@
-package com.itacademy.virtualpet.controller;public class AuthController {
+package com.itacademy.virtualpet.controller;
+
+import com.itacademy.virtualpet.model.User;
+import com.itacademy.virtualpet.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public Mono<ResponseEntity<User>> register(@RequestBody User user) {
+        return authService.register(user.getUsername(), user.getPassword())
+                .map(savedUser -> ResponseEntity.ok((User) savedUser));
+    }
+
+    @PostMapping("/login")
+    public Mono<ResponseEntity<User>> login(@RequestBody User user) {
+        return authService.login(user.getUsername(), user.getPassword())
+                .map(loggedInUser -> ResponseEntity.ok(loggedInUser));
+    }
 }
