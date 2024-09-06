e0bfde8aef4d1d08d9e4da4f18c02ec4211ffcee
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-09-06 17:25:34 +0200

    add ErrorResponse and update GlobalExceptionHandler

--- src/main/java/com/itacademy/virtualpet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itacademy/virtualpet/exception/GlobalExceptionHandler.java
@@ -5,25 +5,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import reactor.core.publisher.Mono;
+import com.itacademy.virtualpet.model.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameAlreadyTakenException.class)
-    public Mono<ResponseEntity<String>> handleUsernameAlreadyTakenException(UsernameAlreadyTakenException error) {
-        return Mono.just(ResponseEntity.status(HttpStatus.CONFLICT).body(error.getMessage()));
+    public Mono<ResponseEntity<ErrorResponse>> handleUsernameAlreadyTakenException(UsernameAlreadyTakenException error) {
+        ErrorResponse errorResponse = new ErrorResponse(error.getMessage(), HttpStatus.CONFLICT.value());
+        return Mono.just(ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public Mono<ResponseEntity<String>> handleUserNotFoundException(UserNotFoundException error) {
-        return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage()));
+    public Mono<ResponseEntity<ErrorResponse>> handleUserNotFoundException(UserNotFoundException error) {
+        ErrorResponse errorResponse = new ErrorResponse(error.getMessage(), HttpStatus.NOT_FOUND.value());
+        return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse));
     }
 
     @ExceptionHandler(Exception.class)
-    public Mono<ResponseEntity<String>> handleGenericException(String message) {
-        if (message.isEmpty()) {
-            return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred."));
-        }
-        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message));
+    public Mono<ResponseEntity<ErrorResponse>> handleGenericException(Exception error) {
+        String message = error.getMessage().isEmpty() ? "An unexpected error occurred." : error.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(message, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse));
     }
 }
+
+
